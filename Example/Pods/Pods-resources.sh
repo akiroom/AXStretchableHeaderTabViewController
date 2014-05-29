7df39fca0959dcbb756ef7888bcb81e68f9e3160
Hiroki Akiyama <aki-hiroki@nifty.com>
2014-05-29 10:30:30 +0900

    reinstall

--- Example/Pods/Pods-resources.sh
+++ Example/Pods/Pods-resources.sh
@@ -41,7 +41,6 @@ install_resource()
       ;;
   esac
 }
-install_resource "../../Classes/AXDummyViewController.xib"
 install_resource "../../Classes/AXStretchableHeaderTabViewController.xib"
 
 rsync -avr --copy-links --no-relative --exclude '*/.svn/*' --files-from="$RESOURCES_TO_COPY" / "${CONFIGURATION_BUILD_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}"
